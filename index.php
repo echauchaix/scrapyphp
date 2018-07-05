f99320e06f3bea04088a6e064bc2bb61d19cd99a
echauchaix <etienne.chauchaix@cnsi-sd.fr>
2018-07-05 18:08:18 +0200

    start working on a modal for the 10 logs of a same flow

--- index.php
+++ index.php
@@ -14,18 +14,28 @@
 			    <th scope="col">Numéro flux</th> 
 			    <th scope="col">Date import</th>
 			    <th scope="col">Ip import</th>
+			    <th scope="col">Voir les 10 derniers</th>
 			</tr>
 		</thead>
 		<tbody>
 	<?php
 		$content = file_get_contents('export.json');
 		$json = json_decode($content, true);
+		$flownum = '';
 		foreach($json as $item) {
-			echo '<tr>';
-			foreach($item as $value) {
-				echo '<td>'.$value.'</td>';
+			//if it is the first log of a flow displays the log in the table
+			if ($item['flownum'] != $flownum) {
+				echo '<tr>';
+				foreach($item as $value) {
+					echo '<td>'.$value.'</td>';
+				}
+				echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#'.$item['flownum'].'Modal">Les 10 derniers</button></td>';
+				echo '</tr>';
+			} else {
+				//else pushes the log in a array
 			}
-			echo '</tr>';
+			$flownum = $item['flownum'];
+			//creates the modal
 		}
 	?>
 		</tbody>
